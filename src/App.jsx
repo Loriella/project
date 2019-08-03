1ef9ff8d1f37ffe7f13367673374eceb9cd0f8a6
Loriella <loris.orlova@gmail.com>
2019-08-03 20:52:39 +0300

    add some styled components

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Login from './pages/login';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Login />
   );
 }
 
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
+
+const FlexContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 8px;
+  display: flex;
+  box-sizing: border-box;
+  
+  justify-content: ${props => props.justify || 'initial'};
+  align-items: ${props => props.align || 'initial'};
+`;
+
+const StyledBox = styled.div`
+  padding: 16px;
+  border: 1px solid #a1eaa4;
+  border-radius: 10px;
+  box-sizing: border-box;
+  
+  min-width: ${props => props.minWidth || 'initial'};
+  ${props => props.colored && css`
+    background: #edf8ed;
+  `}
+`;
+
+const LoginBox = styled(StyledBox)`
+  padding: 32px 16px;
+`;
 
 class Login extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="wrapper">
+      <FlexContainer justify="center" align="center">
+        <LoginBox colored minWidth="320px">
           <form action="">
             <div>
               <h1>Авторизация</h1>
@@ -27,8 +55,8 @@ class Login extends Component {
             <p>Нет аккаунта?</p>
             <a href="/">Зарегистрироваться</a>
           </div>
-        </div>
-      </div>
+        </LoginBox>
+      </FlexContainer>
     );
   }
 }
