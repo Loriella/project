737f27a67c9c9b0710137232b3dd4d87cfb3cc2c
Loriella <loris.orlova@gmail.com>
2019-08-06 16:50:15 +0300

    add Title styled component

---
+++ src/components/styled/Title.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin: 0 0 8px;
+`;
+
+export default Title;
