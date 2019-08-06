1d844e87544d53319ed5b57fd3d588efd06f5bb5
Loriella <loris.orlova@gmail.com>
2019-08-06 16:43:30 +0300

    add FlexContainer styled component

---
+++ src/components/styled/FlexContainer.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const FlexContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 8px;
+  display: flex;
+  box-sizing: border-box;
+  
+  justify-content: ${props => props.justify || 'initial'};
+  align-items: ${props => props.align || 'initial'};
+`;
+
+export default FlexContainer;
