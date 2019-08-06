8452d479afd6d73339c4fec422ba373dc3850860
Loriella <loris.orlova@gmail.com>
2019-08-06 20:27:25 +0300

    removal of the Input component into a separate file

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,26 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-for */
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-const Input = styled.input`
-  border: 1px solid #a1eaa4;
-  border-radius: 10px;
-  padding: 12px 8px;
-  margin: 6px 0;
-  width: 100%;
-  box-sizing: border-box;
-  outline: none;
-  
-  :focus {
-    box-shadow: 0 0 3px 2px rgba(161,234,164,0.55);
-  }
-`;
-
-const InputTitle = styled.div`
-  font-size: 14px;
-  padding: 0 8px;
-`;
+import InputTitle from './styled/InputTitle';
+import Input from './styled/Input';
 
 
 const TextInput = ({
