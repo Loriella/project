09ee42c857e6bf71565977d52a989bf802d1e1c6
Loriella <loris.orlova@gmail.com>
2019-08-06 20:15:56 +0300

    add ErrorText styled component

---
+++ src/components/styled/ErrorText.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ErrorText = styled.div`
+  color: #ff0000;
+  font-size: 14px;
+  text-align: center;
+  margin-bottom: 6px;
+`;
+
+export default ErrorText;
