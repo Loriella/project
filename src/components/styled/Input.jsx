705df1d1afb9c23aecbe7acbbe16c3ab43f5dc7e
Loriella <loris.orlova@gmail.com>
2019-08-06 16:53:58 +0300

    add Input styled component

---
+++ src/components/styled/Input.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  border: 1px solid #a1eaa4;
+  border-radius: 10px;
+  padding: 12px 8px;
+  margin: 6px 0;
+  width: 100%;
+  box-sizing: border-box;
+  outline: none;
+  
+  :focus {
+    box-shadow: 0 0 3px 2px rgba(161,234,164,0.55);
+  }
+`;
+
+export default Input;
