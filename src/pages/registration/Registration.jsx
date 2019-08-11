cbbfbf4aecfa9f0d219537ed251d45df80b3cfe8
Loriella <loris.orlova@gmail.com>
2019-08-11 16:10:17 +0300

    add validation, refactoring code

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -9,94 +9,144 @@ import Title from '../../components/styled/Title';
 
 
 class Registration extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lastName: '',
-      firstName: '',
-      middleName: '',
-      city: '',
-      dateOfBirth: '',
-      login: '',
-      password: '',
-      repeatPassword: '',
-      sexOptions: [
-        {
-          value: 'male',
-          title: 'Мужской',
-        },
-        {
-          value: 'female',
-          title: 'Женский',
-        },
-      ],
-      selectedSex: 'male',
-    };
-    this.loginHandler = this.loginHandler.bind(this);
-    this.passwordHandler = this.passwordHandler.bind(this);
-    this.repeatPasswordHandler = this.repeatPasswordHandler.bind(this);
-    this.lastNameHandler = this.lastNameHandler.bind(this);
-    this.firstNameHandler = this.firstNameHandler.bind(this);
-    this.middleNameHandler = this.middleNameHandler.bind(this);
-    this.cityHandler = this.cityHandler.bind(this);
-    this.dateOfBirthHandler = this.dateOfBirthHandler.bind(this);
-    this.sexOptionsHandler = this.sexOptionsHandler.bind(this);
-  }
+  state = {
+    lastName: '',
+    firstName: '',
+    middleName: '',
+    city: '',
+    dateOfBirth: '',
+    login: '',
+    password: '',
+    repeatPassword: '',
+    sexOptions: [
+      {
+        value: 'male',
+        title: 'Мужской',
+      },
+      {
+        value: 'female',
+        title: 'Женский',
+      },
+    ],
+    selectedSex: 'male',
+    lastNameErrorMessage: '',
+    firstNameErrorMessage: '',
+    middleNameErrorMessage: '',
+    cityErrorMessage: '',
+    loginErrorMessage: '',
+    passwordErrorMessage: '',
+    repeatPasswordErrorMessage: '',
+  };
 
-  loginHandler(e) {
+  loginHandler = (e) => {
     const loginValue = e.target.value;
     this.setState({
       login: loginValue,
+      loginErrorMessage: '',
     });
-  }
+  };
 
-  passwordHandler(e) {
+  passwordHandler = (e) => {
     this.setState({
       password: e.target.value,
+      passwordErrorMessage: '',
     });
-  }
+  };
 
-  repeatPasswordHandler(e) {
+  repeatPasswordHandler = (e) => {
     this.setState({
       repeatPassword: e.target.value,
+      repeatPasswordErrorMessage: '',
     });
-  }
+  };
 
-  lastNameHandler(e) {
+  lastNameHandler = (e) => {
     this.setState({
       lastName: e.target.value,
+      lastNameErrorMessage: '',
     });
-  }
+  };
 
-  firstNameHandler(e) {
+  firstNameHandler = (e) => {
     this.setState({
       firstName: e.target.value,
+      firstNameErrorMessage: '',
     });
-  }
+  };
 
-  middleNameHandler(e) {
+  middleNameHandler = (e) => {
     this.setState({
       middleName: e.target.value,
+      middleNameErrorMessage: '',
     });
-  }
+  };
 
-  cityHandler(e) {
+  cityHandler = (e) => {
     this.setState({
       city: e.target.value,
+      cityErrorMessage: '',
     });
-  }
+  };
 
-  dateOfBirthHandler(e) {
+  dateOfBirthHandler = (e) => {
     this.setState({
       dateOfBirth: e.target.value,
     });
-  }
+  };
 
-  sexOptionsHandler(e) {
+  sexOptionsHandler = (e) => {
     this.setState({
       selectedSex: e.target.value,
     });
-  }
+  };
+
+  commonValidator = errorName => (e) => {
+    if (!e.target.value) {
+      this.setState({
+        [errorName]: 'Заполните поле!',
+      });
+    }
+  };
+
+  loginValidator = (e) => {
+    const loginValue = e.target.value;
+    let errorMessage = '';
+    if (!loginValue) {
+      errorMessage = 'Заполните поле!';
+    } else if (loginValue.length < 3) {
+      errorMessage = 'Слишком короткий логин!';
+    } else if (!/^[a-zA-Z\d]+$/.test(loginValue)) {
+      errorMessage = 'Логин должен содержать символы: 0-9, a-z, A-Z';
+    }
+    this.setState({
+      loginErrorMessage: errorMessage,
+    });
+  };
+
+  passwordValidator = (e) => {
+    const passwordValue = e.target.value;
+    let errorMessage = '';
+    if (!passwordValue) {
+      errorMessage = 'Заполните поле!';
+    } else if (passwordValue.length < 8) {
+      errorMessage = 'Пароль должен содержать не менее 8 символов!';
+    }
+    this.setState({
+      passwordErrorMessage: errorMessage,
+    });
+  };
+
+  repeatPasswordValidator = (e) => {
+    const passwordValue = e.target.value;
+    const { password } = this.state;
+    let errorMessage = '';
+    if (passwordValue !== password) {
+      errorMessage = 'Пароли не совпадают';
+    }
+    this.setState({
+      repeatPasswordErrorMessage: errorMessage,
+    });
+  };
 
   render() {
     const {
@@ -110,6 +160,13 @@ class Registration extends Component {
       dateOfBirth,
       repeatPassword,
       selectedSex,
+      lastNameErrorMessage,
+      firstNameErrorMessage,
+      middleNameErrorMessage,
+      cityErrorMessage,
+      loginErrorMessage,
+      passwordErrorMessage,
+      repeatPasswordErrorMessage,
     } = this.state;
     return (
       <FlexContainer justify="center" align="center">
@@ -123,6 +180,8 @@ class Registration extends Component {
                 name="lastName"
                 handleChange={this.lastNameHandler}
                 placeholder="Иванов"
+                errorMessage={lastNameErrorMessage}
+                handleBlur={this.commonValidator('lastNameErrorMessage')}
               />
               <TextInput
                 title="Имя:"
@@ -130,6 +189,8 @@ class Registration extends Component {
                 name="firstName"
                 handleChange={this.firstNameHandler}
                 placeholder="Василий"
+                errorMessage={firstNameErrorMessage}
+                handleBlur={this.commonValidator('firstNameErrorMessage')}
               />
               <TextInput
                 title="Отчество:"
@@ -137,6 +198,8 @@ class Registration extends Component {
                 name="middleName"
                 handleChange={this.middleNameHandler}
                 placeholder="Петрович"
+                errorMessage={middleNameErrorMessage}
+                handleBlur={this.commonValidator('middleNameErrorMessage')}
               />
               <TextInput
                 title="Дата рождения:"
@@ -151,6 +214,8 @@ class Registration extends Component {
                 name="city"
                 handleChange={this.cityHandler}
                 placeholder="Город"
+                errorMessage={cityErrorMessage}
+                handleBlur={this.commonValidator('cityErrorMessage')}
               />
               <Radio
                 title="Пол"
@@ -165,6 +230,8 @@ class Registration extends Component {
                 inputValue={login}
                 handleChange={this.loginHandler}
                 placeholder="Введите логин"
+                errorMessage={loginErrorMessage}
+                handleBlur={this.loginValidator}
               />
               <TextInput
                 inputType="password"
@@ -173,6 +240,8 @@ class Registration extends Component {
                 inputValue={password}
                 handleChange={this.passwordHandler}
                 placeholder="Введите пароль"
+                errorMessage={passwordErrorMessage}
+                handleBlur={this.passwordValidator}
               />
               <TextInput
                 inputType="password"
@@ -181,6 +250,8 @@ class Registration extends Component {
                 inputValue={repeatPassword}
                 handleChange={this.repeatPasswordHandler}
                 placeholder="Повторите пароль"
+                errorMessage={repeatPasswordErrorMessage}
+                handleBlur={this.repeatPasswordValidator}
               />
               <Button
                 title="Зарегистрироваться"
