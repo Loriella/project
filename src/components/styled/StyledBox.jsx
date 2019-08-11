c189f69877bb25ac9a96740375629ea04460dd3c
Loriella <loris.orlova@gmail.com>
2019-08-11 18:23:01 +0300

    add styles

--- src/components/styled/StyledBox.jsx
+++ src/components/styled/StyledBox.jsx
@@ -1,11 +1,13 @@
 import styled, { css } from 'styled-components';
 
 const StyledBox = styled.div`
-  padding: 8px 16px;
+  padding: 8px 8px;
   border: 1px solid #a1eaa4;
   border-radius: 10px;
   box-sizing: border-box;
+  background: #ffffff;
   
+  max-width: ${props => props.maxWidth || 'initial'};
   min-width: ${props => props.minWidth || 'initial'};
   ${props => props.colored && css`
     background: #edf8ed;
