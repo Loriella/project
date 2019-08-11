86081e34a85b054d4002616fe6289f1946335dff
Loriella <loris.orlova@gmail.com>
2019-08-11 15:36:17 +0300

    changed text-align

--- src/components/styled/ErrorText.js
+++ src/components/styled/ErrorText.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const ErrorText = styled.div`
   color: #ff0000;
-  font-size: 14px;
-  text-align: center;
+  font-size: ${props => props.size || '14px'};
+  text-align: left;
   margin-bottom: 6px;
 `;
 
